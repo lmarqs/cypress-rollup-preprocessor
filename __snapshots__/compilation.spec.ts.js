7babbe8a6960a02db6f64a0b0a63810565462078
lmarqs <marques.analista@gmail.com>
2020-08-17 21:22:25 -0300

    ci: add snapshots to vcs

---
+++ __snapshots__/compilation.spec.ts.js
@@ -0,0 +1,25 @@
+exports['rollup createPreprocessor - e2e correctly preprocesses the file 1'] = `
+(function (factory) {
+  typeof define === 'function' && define.amd ? define(factory) :
+  factory();
+}((function () { 'use strict';
+
+  it('is a test', () => {
+    const [a, b] = [1, 2];
+
+    expect(a).to.equal(1);
+    expect(b).to.equal(2);
+    expect(Math.min(...[3, 4])).to.equal(3);
+  });
+
+})));
+
+`
+
+exports['rollup createPreprocessor - e2e has less verbose "Module not found" error 1'] = `
+Could not resolve './does/not-exist' from test/_test-output/imports_nonexistent_file_spec.js
+`
+
+exports['rollup createPreprocessor - e2e has less verbose syntax error 1'] = `
+Unexpected token
+`

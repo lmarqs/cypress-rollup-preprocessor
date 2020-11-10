a1fddbae1b9a2012b3cdd61055be04c60319487b
lmarqs <lucas.paula@wipro.com>
2020-11-10 02:01:08 -0300

    fix: event emmited on wrong time

--- src/watch.ts
+++ src/watch.ts
@@ -15,11 +15,30 @@ export async function watch (rollupOptions: RollupOptions, outputOptions: Output
 
   file.on('close', createFileClosedListener(watcherKey))
 
-  watcher.on('event', createBuildFinishedListener(file))
-
   return new Promise((resolve, reject) => {
-    watcher.on('event', createOutputEmittedListener(watcherKey, resolve, reject))
+    let firstBuild = true
+
+    watcher.on('event', (e: RollupWatcherEvent) => {
+      if (['BUNDLE_END', 'ERROR'].includes(e.code)) {
+        setCachedWatcherOutput(rollupOptions, e)
+      }
+
+      if (['END', 'ERROR'].includes(e.code)) {
+        if (firstBuild) {
+          firstBuild = false
+          getWatcherCachedOutput(rollupOptions)!
+          .then(resolve)
+          .catch(reject)
+        } else {
+          file.emit('rerun')
+        }
+      }
+    })
   })
+}
+
+function setCachedWatcherOutput(rollupOptions: RollupOptions, e: RollupWatcherEvent) {
+  lastWatchersOutputEmmitedEvents[getWatcherKey(rollupOptions)] = e
 }
 
 function getWatcherKey (rollupOptions: RollupOptions) {
@@ -45,33 +64,5 @@ function createFileClosedListener (watcherKey: string) {
     watchers[watcherKey]?.close()
     delete watchers[watcherKey]
     delete lastWatchersOutputEmmitedEvents[watcherKey]
-  }
-}
-
-function createBuildFinishedListener (file: EventEmitter) {
-  let firstBuild = true
-
-  return (e: RollupWatcherEvent) => {
-    if (['END', 'ERROR'].includes(e.code)) {
-      if (firstBuild) {
-        firstBuild = false
-      } else {
-        file.emit('rerun')
-      }
-    }
-  }
-}
-
-function createOutputEmittedListener (watcherKey: string, resolve: (output: string) => any, reject: (reason: any) => any) {
-  return (e: RollupWatcherEvent) => {
-    if (e.code === 'BUNDLE_END') {
-      lastWatchersOutputEmmitedEvents[watcherKey] = e
-      resolve(e.output[0])
-    }
-
-    if (e.code === 'ERROR') {
-      lastWatchersOutputEmmitedEvents[watcherKey] = e
-      reject(e.error)
-    }
   }
 }
